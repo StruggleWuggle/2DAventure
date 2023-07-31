96ff52d2d0d9f1b1740f06d65f533f767d192f56
StruggleWuggle <brianqre@gmail.com>
2023-07-30 23:59:19 -0700

    saving movement
    Still need to add colliders and collision logic

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,12 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
-    private Vector2 moveDirection;
+    public float collisionOffset = 0.5f;
+    public Vector2 positionRB;
+
+    private Vector2 moveDirection;  // Also facing direction
+    public Vector2 facingDirection;
+
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +29,20 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection= new Vector2(moveX, moveY).normalized;
+        positionRB = rb.position;
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        // Only update facingDirection vector and velocity vector when moveDirection vector is non zero
+        if (math.any(moveDirection))
+        {
+            rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+            facingDirection = new Vector2(moveDirection.x, moveDirection.y);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 }
