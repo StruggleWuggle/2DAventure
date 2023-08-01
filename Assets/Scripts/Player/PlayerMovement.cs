04de297b52489d8f2e3a6c0a4703a1a6db0b22ab
StruggleWuggle <brianqre@gmail.com>
2023-08-01 01:09:01 -0700

    Finished collision
    want to add shake

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,18 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using Random = Unity.Mathematics.Random;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
-    public float collisionOffset = 0.5f;
+    public float collisionOffset;
     public Vector2 positionRB;
+    public ContactFilter2D movementFilter;
 
     private Vector2 moveDirection;  // Also facing direction
     public Vector2 facingDirection;
 
-    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    List<RaycastHit2D> collisionCasts = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
@@ -29,20 +31,45 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection= new Vector2(moveX, moveY).normalized;
-        positionRB = rb.position;
-    }
+        positionRB = rb.position; // Update rigid body position as fast as possible
+}
 
     private void FixedUpdate()
     {
+        bool moveSuccess;
         // Only update facingDirection vector and velocity vector when moveDirection vector is non zero
-        if (math.any(moveDirection))
+        if (moveDirection != Vector2.zero)
         {
-            rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-            facingDirection = new Vector2(moveDirection.x, moveDirection.y);
+            moveSuccess = TryDirection(moveDirection);
+            if (!moveSuccess)
+            {
+                moveSuccess = TryDirection(new Vector2(moveDirection.x, 0));
+                if (!moveSuccess)
+                {
+                    moveSuccess = TryDirection(new Vector2(0, moveDirection.y));
+                }
+            }
         }
         else
         {
             rb.velocity = Vector2.zero;
+        }
+    }
+
+    private bool TryDirection(Vector2 direction)
+    {
+        // Check for potential collisions in given vector direction. If direction is possible, move player and return true
+        int count = rb.Cast(direction, movementFilter, collisionCasts, (moveSpeed * Time.fixedDeltaTime + collisionOffset));
+
+        if(count == 0)
+        {
+            Vector2 currentMove = moveSpeed * direction;
+            rb.velocity = currentMove;
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
