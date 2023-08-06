bdf32f4123329a759b40c67942049a8382a393a8
StruggleWuggle <brianqre@gmail.com>
2023-08-06 05:11:12 -0700

    Failed client side fix

---
+++ Assets/Scripts/HandleStates.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class HandleStates
+{
+    // Start is called before the first frame update
+    public class InputState
+    {
+        public int tick;
+        public float moveX;
+        public float moveY;
+    }
+    public class TransformStateRW : INetworkSerializable
+    {
+        public int tick;
+        public Vector2 finalPosition;
+        public bool isMoving;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            if (serializer.IsReader)
+            {
+                var reader = serializer.GetFastBufferReader();
+                reader.ReadValueSafe(out tick);
+                reader.ReadValueSafe(out finalPosition);
+                reader.ReadValueSafe(out isMoving);
+            }
+            else
+            {
+                var writer = serializer.GetFastBufferWriter();
+                writer.WriteValueSafe(tick);
+                writer.WriteValueSafe(finalPosition);
+                writer.WriteValueSafe(isMoving);
+            }
+        }
+    }
+}
--- Assets/Scripts/NetworkPlayerMovement.cs
+++ Assets/Scripts/NetworkPlayerMovement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NetworkPlayerMovement : NetworkBehaviour
@@ -19,56 +21,216 @@ public class NetworkPlayerMovement : NetworkBehaviour
     public NetworkVariable<Vector2> facingDirection;
     public NetworkVariable<Vector2> playerPosition;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    // Client prediction
+    private int tick = 0;
+    private float tickRate = 1f / 60f;
+    private float tickDeltaTime = 0;
+    private const int buffer = 1024;
 
+    private HandleStates.InputState[] _inputStates = new HandleStates.InputState[buffer];
+    private HandleStates.TransformStateRW[] _transformStates = new HandleStates.TransformStateRW[buffer];
+
+    // For server based rollback
+    public NetworkVariable<HandleStates.TransformStateRW> currentServerTransformState = new NetworkVariable<HandleStates.TransformStateRW>(default, NetworkVariableReadPermission.Everyone);
+    public HandleStates.TransformStateRW previousTransformState;
+
+    private void OnServerStateChanged(HandleStates.TransformStateRW previousState, HandleStates.TransformStateRW serverState)
+    {
+        // Check and reconcile local client predicted movement with server movement
+        if (!IsLocalPlayer)
+        {
+            return;
+        }
+
+        // Edge case for first call where no previous states have been stored yet
+        if (previousTransformState != null)
+        {
+            previousTransformState = serverState;
+        }
+
+        // Check if client and server agree on corresponding tick
+        HandleStates.TransformStateRW calculatedState = _transformStates.First(localState => serverState.tick == localState.tick);
+        if (calculatedState.finalPosition != serverState.finalPosition)
+        {
+            Debug.Log("Correcting client positon");
+            // Then client is out of sync
+            CorrectPlayerPosition(serverState);     // Teleport player at failed tick
+            ReplayMovesAfterTick(serverState);
+
+
+        }
+
+        previousTransformState = previousState;
+    }
+    private void CorrectPlayerPosition(HandleStates.TransformStateRW correctedState)
+    {
+        // Disable character movements TODO
+
+        // Teleport client
+        rb.position = correctedState.finalPosition;
+
+        // Find corresponding state in stored state array based on matching tick and update position value
+        for (int i = 0; i < _transformStates.Length; i++)
+        {
+            if (_transformStates[i].tick == correctedState.tick)
+            {
+                _transformStates[i] = correctedState;
+                break;
+            }
+        }
+    }
+
+    private void ReplayMovesAfterTick(HandleStates.TransformStateRW lastValidState)
+    {
+        // Get all states from stored state array that have a tick value greater than correctedState tick value
+        IDictionary<int, HandleStates.InputState> stateDict = new Dictionary<int, HandleStates.InputState>();
+        int maxTickValue = 0;
+        for (int i = 0; i < _inputStates.Length; i++)
+        {
+            if (_inputStates[i].tick > lastValidState.tick)
+            {
+                // Append dictionary
+                stateDict[_inputStates[i].tick] = _inputStates[i];
+
+                // Update largest tick value encountered
+                if (maxTickValue == 0 || _inputStates[i].tick > maxTickValue)
+                {
+                    maxTickValue = _inputStates[i].tick;
+                }
+            }
+
+        }
+
+        // Execute corresponding input states 
+        for (int i = lastValidState.tick + 1; i <= maxTickValue; i++)
+        {
+            Move(stateDict[i].moveX, stateDict[i].moveY);
+
+            // Get new transform state
+            HandleStates.TransformStateRW updatedTransformState = new HandleStates.TransformStateRW()
+            {
+                tick = stateDict[i].tick,
+                finalPosition = rb.position,
+                isMoving = true,
+            };
+
+            // Update corresponding transform state array
+            for (int j = 0; j < _transformStates.Length; j++)
+            {
+                if (_transformStates[j].tick == stateDict[i].tick)
+                {
+                    _transformStates[j] = updatedTransformState;
+                    break;
+                }
+            }
+        }
+
+    }
+
+    private void OnEnable()
+    {
+        currentServerTransformState.OnValueChanged += OnServerStateChanged;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Check if object has correct ownership
-        if (!IsOwner)
+        if (IsClient && IsLocalPlayer)
         {
-            return;
+            float moveX = Input.GetAxisRaw("Horizontal");
+            float moveY = Input.GetAxisRaw("Vertical");
+            ProcessLocalPlayerMovement(moveX, moveY);
         }
-
-        if (IsServer)
+        else
         {
-            UpdateServer();
-        }
-
-        if (IsClient)
-        {
-            UpdateClient();
+            UpdateOtherPlayers();
         }
     }
-    void UpdateServer()
+    public void ProcessLocalPlayerMovement(float _moveX, float _moveY)
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        tickDeltaTime += Time.deltaTime;
 
+        if (tickDeltaTime > tickRate)
+        {
+            int bufferIndex = tick % buffer;
+
+            MoveServerRpc(_moveX, _moveY, tick);    // Send out move input to server
+            Move(_moveX, _moveY);    // Client side movement only
+
+            // Update states and historic state array
+            HandleStates.InputState inputState = new()
+            {
+                tick = tick,
+                moveX = _moveX,
+                moveY = _moveY,
+            };
+
+            HandleStates.TransformStateRW transformState = new()
+            {
+                tick = tick,
+                finalPosition = rb.position,
+                isMoving = true,
+            };
+            _inputStates[bufferIndex] = inputState;
+            _transformStates[bufferIndex] = transformState;
+
+            // Reduce tick rate back down and prevent overflow of buffer
+            tickDeltaTime -= tickRate;
+            if (tick >= buffer)
+            {
+                tick = 0;
+            }
+            else
+            {
+                tick++;
+            }
+        }
+    }
+    public void Move(float moveX, float moveY)
+    {
         Vector2 movementVector = new Vector2(moveX, moveY);
         rb.velocity = movementVector * MoveSpeed.Value;
     }
-    void UpdateClient()
+
+    public void UpdateOtherPlayers()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        tickDeltaTime += Time.deltaTime;
+        //if (tickDeltaTime > tickRate && currentServerTransformState.Value.isMoving) //
+        if (tickDeltaTime > tickRate)
+        {
+            rb.position = currentServerTransformState.Value.finalPosition;
+            //rb.position = new Vector2(.5f, .5f);
+        }
 
-        // Send out server side movement command
-        MoveServerRpc(moveX, moveY);
-
+        tickDeltaTime -= tickRate;
+        if (tick >= buffer)
+        {
+            tick = 0;
+        }
+        else
+        {
+            tick++;
+        }
     }
 
     // --- ServerRPCs ---
 
     [ServerRpc]
-    public void MoveServerRpc(float moveX, float moveY)
+    public void MoveServerRpc(float moveX, float moveY, int tick)
     {
-        //rb.velocity = Vector2.zero;
-        Vector2 movementVector = new Vector2(moveX, moveY);
-        rb.velocity = movementVector * MoveSpeed.Value;
+        Move(moveX, moveY);
+        HandleStates.TransformStateRW transformState = new()
+        {
+            tick = tick,
+            finalPosition = rb.position,
+            isMoving = true,
+        };
+        // TODO
+        // Check for packet loss by checking if tick != previousTransformState Tick + 1
+        // If missed packet, send packet again
+
+        previousTransformState = currentServerTransformState.Value;
+        currentServerTransformState.Value = transformState;
+
     }
 }
